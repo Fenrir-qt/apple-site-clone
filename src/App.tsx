a14fe19da1114789a02c7b481ab479922e4cca4e
Fenrir-qt <mickburog87@gmail.com>
2025-08-13 08:20:39 +0800

    Remove unused route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Mac from "./pages/Mac";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}/>
-          <Route path="/mac" element={<Mac />}/>
         </Routes>
       </BrowserRouter>
     </>
